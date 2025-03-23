17e32a163a5a75cfc29757afd03e2918d46bd02c
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-23 13:35:17 +0300

    change webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,16 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const isProduction = process.env.NODE_ENV === 'production';
-
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
+    mode: process.env.NODE_ENV || 'development',
+    entry: './src/index.js',// определяет файл или файлы, с которых Webpack начнёт сборку проекта (по умолчанию - ./src/index.js, если не указать)
+    output: {/*определяет выходные параметры:
+                    output.path - Определяет папку, куда Webpack сохранит сгенерированные файлы (по умолчанию - корень проекта)
+                    output.filename - Определяет имя генерируемого бандла (по умолчанию - main.js)*/
+        clean: true, // Очищает папку(output.path) перед сборкой или же очистит все файлы, которые он считает частью сборки.
     },
-    devServer: {
+    devServer: {//запускает сервер, предоставляя автоматическое обновление страницы при изменении кода
         open: true,
         host: 'localhost',
     },
@@ -26,17 +25,34 @@ const config = {
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
             },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-            
             {
                 test: /\.html$/i,
                 use: ['html-loader'],
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: 'url-loader?limit=10000',
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+                use: 'file-loader',
             },
 
             // Add your rules for custom modules here
@@ -45,13 +61,4 @@ const config = {
     },
 };
 
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
-};
+export default config;

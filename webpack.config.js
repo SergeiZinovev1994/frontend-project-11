6195fad34cc867ba6059e5efbfa54610e5531a24
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-31 16:07:51 +0300

    change webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,150 +1,77 @@
-// // Generated using webpack-cli https://github.com/webpack/webpack-cli
-
-// import HtmlWebpackPlugin from 'html-webpack-plugin';
-
-// const config = {
-//     mode: process.env.NODE_ENV || 'development',
-//     entry: './src/index.js',// определяет файл или файлы, с которых Webpack начнёт сборку проекта (по умолчанию - ./src/index.js, если не указать)
-//     output: {/*определяет выходные параметры:
-//                     output.path - Определяет папку, куда Webpack сохранит сгенерированные файлы (по умолчанию - корень проекта)
-//                     output.filename - Определяет имя генерируемого бандла (по умолчанию - main.js)*/
-//         clean: true, // Очищает папку(output.path) перед сборкой или же очистит все файлы, которые он считает частью сборки.
-//     },
-//     devServer: {//запускает сервер, предоставляя автоматическое обновление страницы при изменении кода
-//         open: true,
-//         host: 'localhost',
-//     },
-//     plugins: [
-//         new HtmlWebpackPlugin({
-//             template: 'index.html',
-//         }),
-
-//         // Add your plugins here
-//         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-//     ],
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.js$/,
-//                 exclude: /node_modules/,
-//                 use: {
-//                     loader: 'babel-loader',
-//                     options: {
-//                         presets: ['@babel/preset-env'],
-//                     },
-//                 },
-//             },
-//             {
-//                 test: /\.html$/i,
-//                 use: ['html-loader'],
-//                 exclude: /node_modules/,
-//             },
-//             {
-//                 test: /\.css$/i,
-//                 exclude: /node_modules/,
-//                 use: ['style-loader', 'css-loader', 'postcss-loader'],
-//             },
-//             {
-//                 test: /\.scss$/,
-//                 exclude: /node_modules/,
-//                 use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-//             },
-//             {
-//                 test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-//                 exclude: /node_modules/,
-//                 // use: 'url-loader?limit=10000',
-//             },
-//             {
-//                 test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-//                 exclude: /node_modules/,
-//                 // use: 'file-loader',
-//             },
-
-//             // Add your rules for custom modules here
-//             // Learn more about loaders from https://webpack.js.org/loaders/
-//         ],
-//     },
-// };
-
-// export default config;
-
-// webpack.config.js
-
-
-
-
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const config = {
-mode: process.env.NODE_ENV || 'development',
+  mode: process.env.NODE_ENV || 'development',
 
-entry: './src/index.js',
+  entry: './src/index.js',// определяет файл или файлы, с которых Webpack начнёт сборку проекта (по умолчанию - ./src/index.js, если не указать)
 
-output: {
-path: path.resolve('dist'),
-filename: '[name].[contenthash].js',
-clean: true,
-},
+  output: {/*определяет выходные параметры:
+//                     output.path - Определяет папку, куда Webpack сохранит сгенерированные файлы (по умолчанию - корень проекта)
+//                     output.filename - Определяет имя генерируемого бандла (по умолчанию - main.js)*/
+    path: path.resolve('dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,// Очищает папку(output.path) перед сборкой или же очистит все файлы, которые он считает частью сборки.
+  },
 
-cache: {
-type: 'filesystem', // ускоряет повторные сборки
-},
+  cache: {
+    type: 'filesystem', // ускоряет повторные сборки
+  },
 
-devServer: {
-open: true,
-host: 'localhost',
-port: 3000,
-hot: true,
-compress: true,
-client: {
-overlay: true,
-logging: 'warn',
-},
-},
+  devServer: {
+    open: true,// Автоматически открывает браузер после запуска сервера.
+    host: 'localhost',
+    port: 3000,
+    hot: true,// Включает "горячую перезагрузку" (Hot Module Replacement), чтобы изменения кода применялись без перезагрузки страницы.
+    compress: true,// Включает сжатие HTTP-ответов для повышения производительности.
+    client: {
+      overlay: true,// Показывает ошибки компиляции или runtime-ошибки прямо на странице в виде оверлея.
+      logging: 'warn',// Настраивает уровень логирования клиента, отображая предупреждения (`warn`) в консоли.
+    },
+  },
 
-plugins: [
-new HtmlWebpackPlugin({
-template: 'index.html',
-}),
-],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
 
-module: {
-rules: [
-{
-test: /\.js$/,
-include: path.resolve('src'),
-exclude: /node_modules/,
-use: {
-loader: 'babel-loader',
-options: {
-presets: ['@babel/preset-env'],
-cacheDirectory: true, // кешируем компиляцию babel
-},
-},
-},
-{
-test: /\.html$/i,
-include: path.resolve('src'),
-use: ['html-loader'],
-},
-{
-test: /\.css$/i,
-include: path.resolve('src'),
-use: ['style-loader', 'css-loader'],
-},
-{
-test: /\.scss$/,
-include: path.resolve('src'),
-use: ['style-loader', 'css-loader', 'sass-loader'],
-},
-{
-test: /\.(woff2?|ttf|eot|svg)$/,
-type: 'asset/resource',
-include: path.resolve('src'),
-},
-],
-},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,// Проверяет файлы, расширение которых '.js'. Применяет загрузчик только к таким файлам.
+        include: path.resolve('src'),// Указывает папку 'src', в которой будут обрабатываться файлы. Это ограничивает область применения загрузчика.
+        exclude: /node_modules/,// Исключает файлы в папке 'node_modules', чтобы ускорить сборку и избежать конфликтов с готовыми библиотеками.
+        use: {
+          loader: 'babel-loader',// Использует Babel для преобразования JavaScript-кода в совместимый со старыми браузерами формат.
+          options: {
+            presets: ['@babel/preset-env'],// Указывает пресет для преобразования ES6+ синтаксиса в ES5 для совместимости с браузерами.
+            cacheDirectory: true, // Включает кэширование компиляции Babel, что ускоряет повторные сборки, сохраняя результаты предыдущих.
+          },
+        },
+      },
+      {
+        test: /\.html$/i,
+        include: path.resolve('src'),
+        use: ['html-loader'],
+      },
+      {
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        include: path.resolve('src'),
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(woff2?|ttf|eot|svg)$/,
+        type: 'asset/resource',
+        include: path.resolve('src'),
+      },
+    ],
+  },
 };
 
 export default config;
